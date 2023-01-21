5af11d8b5fedb1f220b4e94a1064ce8954e295b0
Josh Hayles <josh@joshhayles.com>
2023-01-21 14:33:13 -0700

    DSAs

--- 05_problems/ByName/04_compress.js
+++ 05_problems/ByName/04_compress.js
@@ -18,5 +18,22 @@
 // // -> '127y'
 
 const compress = (s) => {
+    let i = 0;
+    let j = 0;
+    let result = [];
 
+    while (j <= s.length) {
+        if (s[i] === s[j]) {
+            j += 1;
+        } else {
+            let num = j - i;
+            if (num === 1) {
+                result.push(s[i]);
+            } else {
+                result.push(num + s[i]);
+            }
+            i = j;
+        }
+    }
+    return result.join('');
 };
